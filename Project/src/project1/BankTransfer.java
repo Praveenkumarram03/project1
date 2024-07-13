e8831f5cab864e83022d03b3f92e4473064028d7
Praveenkumar <146968403+Praveenkumarram03@users.noreply.github.com>
2024-07-13 11:46:39 +0530

    Update BankTransfer.java

--- Project/src/project1/BankTransfer.java
+++ Project/src/project1/BankTransfer.java
@@ -1,14 +1,17 @@
 package project1;
-
 import java.util.Scanner;
 
 public class BankTransfer {
+    String fromAcc;
+    String toAcc;
+    double amount;
+    String branch;
+    double fromAccBalance = 10000;
+    Scanner scanner;
 
-    private String fromAcc;
-    private String toAcc;
-    private double amount;
-    private String branch;
-    private double fromAccBalance = 10000;
+    public BankTransfer() {
+        scanner = new Scanner(System.in);
+    }
 
     public void checkBalance() {
         System.out.println("Current balance in account " + fromAcc + ": " + fromAccBalance);
@@ -23,38 +26,47 @@ public class BankTransfer {
         }
     }
 
+    public void startTransaction() {
+        System.out.println(fromAcc);
+        System.out.println(toAcc);
+        System.out.println(branch);
+
+        while (true) {
+            System.out.println("1. Check Balance");
+            System.out.println("2. Fund Transfer");
+            System.out.println("0. Exit");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    System.out.print("Enter amount to transfer: ");
+                    amount = scanner.nextDouble();
+                    fundTransfer();
+                    checkBalance();
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BankTransfer transaction = new BankTransfer();
         transaction.fromAcc = "12345";
         transaction.toAcc = "67890";
         transaction.branch = "SBI Avaniyapuram Main Branch";
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println(transaction.fromAcc);
-        System.out.println(transaction.toAcc);
-        System.out.println(transaction.branch);
-        System.out.println("1. Check Balance");
-        System.out.println("2. Fund Transfer");
-        System.out.println("0. Exit");
-
-        System.out.print("Enter your choice: ");
-        int choice = scanner.nextInt();
-
-    switch (choice) {
-            case 1:
-            	System.out.print("Enter amount to transfer: ");
-                transaction.amount = scanner.nextDouble();
-                transaction.fundTransfer();
-              
-            case 2:
-            	  transaction.checkBalance();
-            	  break;
-               
-                 default:
-                System.out.println("Invalid choice");
-                break;
-        }
-
+        
+        transaction.startTransaction();
     }
 }
+
